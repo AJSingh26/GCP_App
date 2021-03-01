e5ab11267e93c8e86031b295bde97d70aeb47dd1
GCP Web Dev <theo.s.urban@gmail.com>
2021-02-28 22:48:03 -0500

    First Commit, Includes unlinked splash and main pages.

---
+++ GCP/colorExt.swift
@@ -0,0 +1,16 @@
+//
+//  colorExt.swift
+//  GCP
+//
+//  Created by urban on 2/28/21.
+//
+
+import Foundation
+
+import SwiftUI
+
+extension Color {
+    public static var GCP_Green: Color {
+        return Color(UIColor(red: 20/255, green: 135/255, blue: 53/255, alpha: 1.0))
+    }
+}
