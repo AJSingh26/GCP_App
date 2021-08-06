7a57973202001c54ac261381e0e38b76b1cb964b
Theo Urban <theourban@Admins-MacBook-Air.local>
2021-08-06 15:43:43 -0400

    First commit of sBoards)

---
+++ GCP_SBOARD/CustomStaticCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomStaticCellTableViewCell.swift
+//  GCP_SBOARD
+//
+//  Created by Theo Urban on 7/28/21.
+//
+
+import UIKit
+
+class CustomStaticCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ GCP_SBOARD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCP_SBOARD
+//
+//  Created by urban on 3/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
